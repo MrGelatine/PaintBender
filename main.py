ea43389edce664cbc4b939559e4a232d38eb2a84
MrGelatine <52462529+MrGelatine@users.noreply.github.com>
2022-08-20 13:54:56 +0300

    Add conda requirements

--- main.py
+++ main.py
@@ -3,12 +3,12 @@ from torchvision import transforms, models
 import torch.optim as optim
 import torch
 import time
-
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 PHOTO_PATH = r"./Mona_Lisa.jpg"
 STYLE_PATH = r"./Sky.jpg"
 kernel = models.vgg19(pretrained=True).features
-model = paintBenderModel.PaintBender(core= kernel, target_path= PHOTO_PATH, style_path= STYLE_PATH, device= device, time_limit=1, snap_every=1000)
+model = paintBenderModel.PaintBender(core= kernel, target_path= PHOTO_PATH, style_path= STYLE_PATH,
+                                     device= device, time_limit=1, snap_every=1000)
 tar = model.create_target()
 
 optimizer = optim.Adam([tar], lr=0.03)
